3ebc03b20b87026d21f6929b210b3ff76d381355
mohdzeeshan2197 <m.zeeshan2197@gmail.com>
2023-02-26 17:40:27 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify  
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+@app.route("/",methods=['GET','POST'])
+def home_page():
+    return render_template('index.html')
+
+@app.route("/math",methods=['POST'])
+def math_operation():
+    if (request.method == "POST"):
+        ops = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+
+        if ops =='add':
+            r = num1 + num2
+            result= "The Sum of "+str(num1)+" and "+str(num2)+" is "+str(r)
+            return render_template('results.html',result = result)
+
+        if ops =='subtract':
+            r = num1 - num2
+            result= "The Substraction of "+str(num1)+" and "+str(num2)+" is "+str(r)
+            return render_template('results.html',result=result)
+
+        if ops =='multiply':
+            r = num1 *num2
+            result= "The Multiplication of "+str(num1)+" and "+str(num2)+" is "+str(r)
+            return render_template('results.html',result = result)
+
+        if ops =='divide':
+            r = num1/num2
+            result= "The Division of"+str(num1)+" and "+str(num2)+" is "+str(r)
+            return render_template('results.html',result= result)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
